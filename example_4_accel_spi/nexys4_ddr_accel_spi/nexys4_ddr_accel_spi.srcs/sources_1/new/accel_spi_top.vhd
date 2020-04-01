c9041ba648d5823cd7d8d29521751ac27b338ca4
Eric Biazo <eric.l.biazo@gmail.com>
2020-03-31 20:40:10 -0400

    got the vga moving the red controller via accel

--- example_4_accel_spi/nexys4_ddr_accel_spi/nexys4_ddr_accel_spi.srcs/sources_1/new/accel_spi_top.vhd
+++ example_4_accel_spi/nexys4_ddr_accel_spi/nexys4_ddr_accel_spi.srcs/sources_1/new/accel_spi_top.vhd
@@ -41,7 +41,10 @@ entity accel_spi_top is
             BTNL : in STD_LOGIC;
             BTNR : in STD_LOGIC;
             BTNU : in STD_LOGIC;
-            BTND : in STD_LOGIC;        
+            BTND : in STD_LOGIC;     
+               
+            -- Switches
+            SW : in std_logic_vector(15 downto 0);
             
             -- Clock
             CLK100MHZ : in STD_LOGIC;
@@ -79,6 +82,12 @@ architecture Behavioral of accel_spi_top is
     signal vga_x : integer := 0;
     signal vga_y : integer := 0;
 
+    signal vga_x_btn : integer := 0;
+    signal vga_y_btn : integer := 0;
+
+    signal vga_x_accel : integer := 0;
+    signal vga_y_accel : integer := 0;
+
     signal DATA_X : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
     signal DATA_Y : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
     signal DATA_Z : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
@@ -87,37 +96,54 @@ architecture Behavioral of accel_spi_top is
     
     signal vga_x_slv : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
     signal vga_y_slv : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
-begin
 
-     vga_driver_ins : entity vga_driver port map(
-             clk    => CLK100MHZ,
-             reset  => BTNC,
-             vga_x => vga_x,
-             vga_y => vga_y,
-             vga_hs => VGA_HS,
-             vga_vs => VGA_VS,
-             vga_r  => VGA_R,
-             vga_g  => VGA_G,
-             vga_b  => VGA_B
+    signal char_7 : std_logic_vector(3 downto 0) := (others => '0');
+    signal char_6 : std_logic_vector(3 downto 0) := (others => '0');
+    signal char_5 : std_logic_vector(3 downto 0) := (others => '0');
+    signal char_4 : std_logic_vector(3 downto 0) := (others => '0');
+    signal char_3 : std_logic_vector(3 downto 0) := (others => '0');
+    signal char_2 : std_logic_vector(3 downto 0) := (others => '0');
+    signal char_1 : std_logic_vector(3 downto 0) := (others => '0');
+    signal char_0 : std_logic_vector(3 downto 0) := (others => '0');
+begin
+    vga_x_slv <= std_logic_vector(to_unsigned(vga_x, vga_x_slv'length));
+    vga_y_slv <= std_logic_vector(to_unsigned(vga_y, vga_y_slv'length));
+
+    char_7 <= DATA_X(7 downto 4);
+    char_6 <= DATA_X(3 downto 0);
+    char_5 <= DATA_Y(7 downto 4);
+    char_4 <= DATA_Y(3 downto 0);
+    char_3 <= vga_x_slv(7 downto 4);
+    char_2 <= vga_x_slv(3 downto 0);
+    char_1 <= vga_y_slv(7 downto 4);
+    char_0 <= vga_y_slv(3 downto 0);
+
+    vga_driver_ins : entity vga_driver port map(
+            clk    => CLK100MHZ,
+            reset  => BTNC,
+            vga_x => vga_x,
+            vga_y => vga_y,
+            vga_hs => VGA_HS,
+            vga_vs => VGA_VS,
+            vga_r  => VGA_R,
+            vga_g  => VGA_G,
+            vga_b  => VGA_B
          );
         
-     vga_red_controller_ins : entity vga_red_controller port map(
-             CLK100MHZ     => CLK100MHZ,         
-             LED           => LED,      
-             SEG7_CATH     => SEG7_CATH,
-             AN            => AN,       
-             vga_x         => vga_x,    
-             vga_y         => vga_y,
-             BTNC          => BTNC,
-             BTNL          => BTNL,
-             BTNR          => BTNR,
-             BTNU          => BTNU,
-             BTND          => BTND
-         );
-   
-	accel_i : entity accel_spi_rw port map(
-            clk => CLK100MHZ,
-            reset =>  BTNC,
+     vga_red_controller_ins : entity vga_red_controller
+        port map(
+            CLK100MHZ     => CLK100MHZ,         
+            LED           => LED,      
+            SEG7_CATH     => SEG7_CATH,
+            AN            => AN,       
+            vga_x         => vga_x,    
+            vga_y         => vga_y,
+            BTNC          => BTNC,
+            BTNL          => BTNL,
+            BTNR          => BTNR,
+            BTNU          => BTNU,
+            BTND          => BTND,
+            SW            => SW,
             --Values from Accel
             DATA_X => DATA_X,
             DATA_Y => DATA_Y,
@@ -130,21 +156,22 @@ begin
             SCLK => ACL_SCLK,
             MISO => ACL_MISO
         );
+   
         
-        vga_x_slv <= std_logic_vector(to_unsigned(vga_x, vga_x_slv'length));
-        vga_y_slv <= std_logic_vector(to_unsigned(vga_y, vga_y_slv'length));
+
         
         seg7_controller_i : entity seg7_controller 
         port map( clk => CLK100MHZ,
                   reset => btnc,
-                  char_7 => ID_AD(7 downto 4),
-                  char_6 => ID_AD(3 downto 0),
-                  char_5 => ID_1D(7 downto 4),
-                  char_4 => ID_1D(3 downto 0),
-                  char_3 => DATA_X(7 downto 4),
-                  char_2 => DATA_X(3 downto 0),
-                  char_1 => DATA_Y(7 downto 4),
-                  char_0 => DATA_Y(3 downto 0),
+                  char_7 => char_7, 
+                  char_6 => char_6,
+                  char_5 => char_5,
+                  char_4 => char_4,
+                  char_3 => char_3,
+                  char_2 => char_2,
+                  char_1 => char_1,
+                  char_0 => char_0,
                   seg7_cath => SEG7_CATH,
-                  an => AN);
+                  an => AN
+                );
 end Behavioral;
--- example_4_accel_spi/nexys4_ddr_accel_spi/nexys4_ddr_accel_spi.srcs/sources_1/new/vga_red_controller.vhd
+++ example_4_accel_spi/nexys4_ddr_accel_spi/nexys4_ddr_accel_spi.srcs/sources_1/new/vga_red_controller.vhd
@@ -47,7 +47,19 @@ entity vga_red_controller is
             BTNL : in STD_LOGIC;
             BTNR : in STD_LOGIC;
             BTNU : in STD_LOGIC;
-            BTND : in STD_LOGIC
+            BTND : in STD_LOGIC;
+
+            SW : in STD_LOGIC_VECTOR(15 DOWNTO 0);
+
+            DATA_X : out STD_LOGIC_VECTOR(7 downto 0);
+            DATA_Y : out STD_LOGIC_VECTOR(7 downto 0);
+            DATA_Z : out STD_LOGIC_VECTOR(7 downto 0);
+            ID_AD : out STD_LOGIC_VECTOR(7 downto 0);
+            ID_1D : out STD_LOGIC_VECTOR(7 downto 0);
+            CSb : out STD_LOGIC;
+            MOSI : out STD_LOGIC;
+            SCLK : out STD_LOGIC;
+            MISO : in STD_LOGIC
         );
 end vga_red_controller;
 
@@ -75,6 +87,15 @@ architecture Behavioral of vga_red_controller is
     signal btnr_enable : std_logic := '0';
     signal btnd_enable : std_logic := '0';
     signal btnl_enable : std_logic := '0';
+
+    signal accel_u_enable : std_logic := '0';
+    signal accel_r_enable : std_logic := '0';
+    signal accel_d_enable : std_logic := '0';
+    signal accel_l_enable : std_logic := '0';
+
+    signal data_x_t : STD_LOGIC_VECTOR(7 downto 0) := (others=>'0');
+    signal data_y_t : STD_LOGIC_VECTOR(7 downto 0) := (others=>'0');
+
 begin
     ------------------------------------------------------
     -- Debounce Buttons
@@ -156,18 +177,74 @@ begin
             last_inc := debounced_btnl;
         end if;
     end process;
-    
-                  
+   
+    detect_accel_u: process(CLK100MHZ)
+        variable accel_reset : std_logic := '0';
+    begin
+        if(rising_edge(CLK100MHZ)) then
+            accel_u_enable <= '0';
+            if(data_y_t = x"00") then
+                accel_reset := '0';
+            elsif(accel_reset = '0' and data_y_t = x"E5") then
+                accel_reset := '1';
+                accel_u_enable <= '1';
+            end if;
+        end if;
+    end process;
+
+    detect_accel_r: process(CLK100MHZ)
+        variable accel_reset : std_logic := '0';
+    begin
+        if(rising_edge(CLK100MHZ)) then
+            accel_r_enable <= '0';
+            if(data_x_t = x"00") then
+                accel_reset := '0';
+            elsif(accel_reset = '0' and data_x_t = x"E5") then
+                accel_reset := '1';
+                accel_r_enable <= '1';
+            end if;
+        end if;
+    end process;
+
+    detect_accel_d: process(CLK100MHZ)
+        variable accel_reset : std_logic := '0';
+    begin
+        if(rising_edge(CLK100MHZ)) then
+            accel_d_enable <= '0';
+            if(data_y_t = x"00") then
+                accel_reset := '0';
+            elsif(accel_reset = '0' and data_y_t = x"1B") then
+                accel_reset := '1';
+                accel_d_enable <= '1';
+            end if;
+        end if;
+    end process;
+
+    detect_accel_l: process(CLK100MHZ)
+        variable accel_reset : std_logic := '0';
+    begin
+        if(rising_edge(CLK100MHZ)) then
+            accel_l_enable <= '0';
+            if(data_x_t = x"00") then
+                accel_reset := '0';
+            elsif(accel_reset = '0' and data_x_t = x"1B") then
+                accel_reset := '1';
+                accel_l_enable <= '1';
+            end if;
+        end if;
+    end process;
+
+    -- Calculates the red box index.              
     set_seg_proc : process(CLK100MHZ)
+        variable last_data_x_t : std_logic_vector(7 downto 0) := x"00";
     begin
         -- When BTNC is HIGH, set every characters to 0. Reset.
-        if(rising_edge(CLK100MHZ)) then
-            if(BTNC = '1') then
-                vga_y_t <= (others=>'0');
-                vga_x_t <= (others=>'0');
-            else
+        if(BTNC = '1') then
+            vga_y_t <= (others=>'0');
+            vga_x_t <= (others=>'0');
+        elsif(rising_edge(CLK100MHZ)) then
+            if(SW(1) = '0') then
                 if(btnu_enable = '1') then
-
                     if(vga_y_t = b"0000") then -- if it was all 0 then make it 0xe
                         vga_y_t <= x"e";
                     else
@@ -182,7 +259,7 @@ begin
                         vga_x_t <= vga_x_t + 1;
                     end if;
                 end if;
-                
+                    
                 if(btnd_enable = '1') then
                     if(vga_y_t = x"e") then -- if it was 0xe then set it all 0
                         vga_y_t <= (others=>'0');
@@ -190,8 +267,40 @@ begin
                         vga_y_t <= vga_y_t + 1;
                     end if;
                 end if;
-                
+        
                 if(btnl_enable = '1') then
+                    if(vga_x_t = b"00000") then
+                        vga_x_t <= b"10011"; -- Set ut as 0x13
+                    else
+                        vga_x_t <= vga_x_t - 1;
+                    end if;
+                end if;
+            elsif(SW(1) = '1') then-- Accelerameter Controls
+                if(accel_u_enable = '1') then
+                    if(vga_y_t = b"0000") then -- if it was all 0 then make it 0xe
+                        vga_y_t <= x"e";
+                    else
+                        vga_y_t <= vga_y_t - 1;
+                    end if;
+                end if;
+
+                if(accel_r_enable = '1') then
+                    if(vga_x_t = b"10011") then -- if it was 0x13 set it 0
+                        vga_x_t <= (others=>'0');
+                    else
+                        vga_x_t <= vga_x_t + 1;
+                    end if;
+                end if;
+                    
+                if(accel_d_enable = '1') then
+                    if(vga_y_t = x"e") then -- if it was 0xe then set it all 0
+                        vga_y_t <= (others=>'0');
+                    else
+                        vga_y_t <= vga_y_t + 1;
+                    end if;
+                end if;
+        
+                if(accel_l_enable = '1') then
                     if(vga_x_t = b"00000") then
                         vga_x_t <= b"10011"; -- Set ut as 0x13
                     else
@@ -201,6 +310,27 @@ begin
             end if;
         end if;
     end process;
+
+
+    accel_i : entity accel_spi_rw
+    port map(
+        clk => CLK100MHZ,
+        reset =>  BTNC,
+        --Values from Accel
+        DATA_X => data_y_t,
+        DATA_Y => data_x_t,
+        DATA_Z => DATA_Z,
+        ID_AD => ID_AD,
+        ID_1D => ID_1D,
+        --SPI Signals
+        CSb => CSb,
+        MOSI => MOSI,
+        SCLK => SCLK,
+        MISO => MISO
+    );
+
+    DATA_X <= data_x_t;
+    DATA_Y <= data_y_t;
 
     vga_x <= to_integer(vga_x_t);
     vga_y <= to_integer(vga_y_t);
